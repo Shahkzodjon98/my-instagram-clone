e6a15cc03c5864790ba0fa6b332bfb6c540ab7f9
Shakhzod <105542321+Shahkzodjon98@users.noreply.github.com>
2023-02-18 16:19:57 +0500

    Update config.js

--- src/IIb/config.js
+++ src/IIb/config.js
@@ -6,13 +6,7 @@ import { getStorage } from "firebase/storage";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDeoQximL4xMmTS9V0gm3QRY1hOXNAGTpw",
-  authDomain: "instagram-bcd7d.firebaseapp.com",
-  projectId: "instagram-bcd7d",
-  storageBucket: "instagram-bcd7d.appspot.com",
-  messagingSenderId: "193696635560",
-  appId: "1:193696635560:web:5e3352da6184d3e251c09d",
-  measurementId: "G-6P27C9CMZ9"
+ 
 };
 
 const app = initializeApp(firebaseConfig);
